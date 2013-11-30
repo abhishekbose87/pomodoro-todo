cf7af2369ab240f5c76be71c3f49c1faed80de42
Abhishek <abhishekbose87@gmail.com>
2013-11-30 18:09:13 +0530

    Create a popup when time finishes

--- vendor/assets/javascripts/example.js
+++ vendor/assets/javascripts/example.js
@@ -1,5 +1,5 @@
 var clock;
-var pomodoroTime= 1800;
+var pomodoroTime= 3;
 
 			$(document).ready(function() {
 
@@ -9,7 +9,31 @@ var pomodoroTime= 1800;
 		        autoStart: false,
 		        callbacks: {
 		        	stop: function() {
-		        		$('.message').html('The clock has stopped!');
+		        	//	$('.message').html('Did you successfully complete a pomodoro ?? ');
+//alert('Did you successfully complete a pomodoro ?? ');
+                                     // alertEndofPomodoro();
+                                     //alert('Did you successfully complete a pomodoro ?? ');
+  if (window.webkitNotifications.checkPermission() == 0) { // 0 is PERMISSION_ALLOWED
+    // function defined in step 2
+    //alert('Did you successfully complete a pomodoro ?? ');
+   
+
+ notification = window.webkitNotifications.createNotification(
+      'http://i.stack.imgur.com/dmHl0.png', 'EXECUTE notification!',
+      'Have you finished one pomodoro of work');
+    //notification.ondisplay = function() { ... do something ... };
+    //notification.onclose = function() { ... do something else ... };
+
+    notification.onclick = function () {
+      window.focus();
+      //window.open("http://stackoverflow.com/a/13328397/1269037");
+      notification.close();
+    }    
+
+    notification.show();
+  } else {
+    window.webkitNotifications.requestPermission();
+  }
 		        	}
 		        }
         		});
@@ -18,7 +42,7 @@ var pomodoroTime= 1800;
 
                   // Attach a click event to a button a increment the clock
 				$('.start').click(function() {
-                                        clock.setTime(1800);
+                                        clock.setTime(pomodoroTime);
                                         clock.setCountdown(true);
 					clock.start();
 				});
@@ -26,3 +50,28 @@ var pomodoroTime= 1800;
 				$('.stop').click(function() {
 					clock.stop();
 				});
+
+// check for notifications support
+// you can omit the 'window' keyword
+if (window.webkitNotifications) {
+  console.log("Notifications are supported!");
+}
+else {
+  console.log("Notifications are not supported for this Browser/OS version yet.");
+}
+
+function alertEndofPomodoro()
+{
+  alert('Did you successfully complete a pomodoro ?? ');
+  if (window.webkitNotifications.checkPermission() == 0) { // 0 is PERMISSION_ALLOWED
+    // function defined in step 2
+    notification_test = window.webkitNotifications.createNotification(
+      'http://i.stack.imgur.com/dmHl0.png', 'Chrome notification!',
+      'Here is the notification text');
+    //notification_test.ondisplay = function() { ... do something ... };
+    //notification_test.onclose = function() { ... do something else ... };
+    notification_test.show();
+  } else {
+    window.webkitNotifications.requestPermission();
+  }
+}
